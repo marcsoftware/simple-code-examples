fa01b7db3eec7c3626ab607dec4fcb8bc9d289dc
marcsoftware <marcdavidmelcher@gmail.com>
2021-01-14 16:36:12 -0600

    cold.html in progress

--- lib.js
+++ lib.js
@@ -57,3 +57,25 @@ function shuffle(array) {
         array[j] = temp;
     }
 }
+
+
+//-----------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------
+function getCookie(cname) {
+  var name = file_name+cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+
+
+  return 0;
+}
